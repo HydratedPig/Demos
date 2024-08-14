17410f41a2fa25533b2e3d5a3f0baacc478abbcb
HydratedPig <1162183871@qq.com>
2024-08-14 20:04:18 +0800

    feat: 修改一下

--- app.vue
+++ app.vue
@@ -1,31 +1,10 @@
 <template>
-  <span>{{ timeLabel }}</span>
+  <Comp/>
+  <Comp/>
 </template>
 
 <script lang="ts" setup>
-import en from './locales/en.json';
-const useInit = () => {
-  const _message = ref(en);
-  const messages = computed(() => _message.value);
-  const t = (s: string) => {
-    const sArr = s.split('.');
-    const res = sArr.reduce((cur: string | object | any[], i: any) => {
-      if (typeof cur !== 'string' && (cur as any)[i]) {
-        return (cur as any)[i]
-      }
-      return cur
-    }, messages.value)
-    return typeof res === 'string' ? res : '--'
-  }
-  return {
-    t
-  }
-}
-
-const $i18n = useInit();
-
-await new Promise(resolve => setTimeout(resolve, 10))
-
-const timeLabel = computed(() => $i18n.t('general.body.static'))
+import Comp from './Comp.vue'
+console.log('request')
 </script>
 
